1b550fdc83d36af534d2a82735d1f0868973abf6
Steven Martreux <steven.martreux@epitech.eu>
2017-09-21 14:24:49 +0200

    Add ListRestRoomTableViewCell

---
+++ FindYourRestroom/UI/Cell/ListRestRoomTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ListRestRoomTableViewCell.swift
+//  FindYourRestroom
+//
+//  Created by Martreux Steven on 21/09/2017.
+//  Copyright © 2017 me. All rights reserved.
+//
+
+import UIKit
+
+class ListRestRoomTableViewCell: UITableViewCell {
+
+    static var identifier = "ListRestRoomTableViewCell"
+    
+}
