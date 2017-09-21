8127a9631a3aaa5d62d9a919ee6e554358ad3a70
Steven Martreux <steven.martreux@epitech.eu>
2017-09-21 17:25:40 +0200

    Handle request inside LoadingViewController and add Icon Application

--- FindYourRestroom/UI/Loading/LoadingViewController.swift
+++ FindYourRestroom/UI/Loading/LoadingViewController.swift
@@ -9,8 +9,49 @@
 import UIKit
 
 class LoadingViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    @IBOutlet weak var centerImageConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imgLoading: UIImageView!
+    @IBOutlet weak var loadingLabel: UILabel!
+    @IBOutlet weak var loader: UIActivityIndicatorView!
+    
+    var loadingModel = LoadingViewModel()
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        startAnimation()
+    }
+    
+    func startAnimation() {
+        self.centerImageConstraint.constant -= self.imgLoading.frame.size.height
+        UIView.animate(withDuration: 0.8, animations: {
+            self.view.layoutIfNeeded()
+            self.loadingLabel.alpha = 1
+        }) { (_) in
+            self.startRequest()
+        }
+    }
+    
+    func startRequest() {
+        loader.startAnimating()
+        loadingModel.fetchRestRoom {[weak self] (success) in
+            guard let strongSelf = self else {
+                return
+            }
+            
+            if success {
+                NavigationManager.showMainTabBar()
+            } else {
+                strongSelf.showError()
+            }
+        }
+    }
+    
+    func showError() {
+        let alert = UIAlertController(title: "Error", message: "It seems you don't any connection", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Retry", style: .default, handler: { (_) in
+            self.startRequest()
+        }))
+        self.present(alert, animated: true, completion: nil)
     }
 }
